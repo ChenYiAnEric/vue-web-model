0e54f89fb0e0e2f0238e97d97aafc246366b5122
DESKTOP-Eric <mopack80684@gmail.com>
2021-07-09 16:33:47 +0800

    install eslint bootstrap prettier vuex vue-router set bootstrap eslint prettier

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src="../assets/header/Logo_mockup.png"
+            alt=""
+            width="35"
+            height="35"
+          />
+          Bootstrap
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link class="nav-link active" to="/">Home</router-link>
+            <router-link class="nav-link" to="/about">About</router-link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+};
+</script>
